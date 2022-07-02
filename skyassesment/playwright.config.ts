0b774904a8462e5e6e42749ed2c777367f3b23cf
Subroto Singh <subroto2040@gmail.com>
2022-07-02 15:32:04 +0530

    Automation test
    Automation test initial commit

---
+++ skyassesment/playwright.config.ts
@@ -0,0 +1,33 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  testDir: './tests',
+  /* Maximum time one test can run for. */
+  timeout: 60 * 10000,
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in `await expect(locator).toHaveText();`
+     */
+    timeout: 10000000
+  },
+  
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    browserName: 'chromium',
+    channel: 'chrome',
+    headless: false,
+    screenshot: 'on',
+    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+    actionTimeout: 0,
+    trace: 'on-first-retry',
+    permissions:['geolocation'],
+  },
+  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
+  outputDir: 'test-results/',
+
+};
+
+export default config;
