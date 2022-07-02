f1ece5777f93eb4d6b1fefef845a14c1e591d602
Subroto Singh <subroto2040@gmail.com>
2022-07-02 20:02:53 +0530

    Update shopPageNavigationTest.spec.ts
    updated

--- skyassesment/tests/shopPageNavigationTest.spec.ts
+++ skyassesment/tests/shopPageNavigationTest.spec.ts
@@ -13,7 +13,7 @@ test.describe(
     });
 
     // Scenario 1: User navigates to shop page
-    test("verify user navigates to shop page", async ({ page }) => {
+    test("verify the navigation on Deals page", async ({ page }) => {
       // Go To Deals --> Click [data-test-id="primary-nav-list"] >> text=Deals
       await page
         .locator('[data-test-id="primary-nav-list"] >> text=Deals')
@@ -22,7 +22,7 @@ test.describe(
     });
 
     // Scenario 2: User sees tiles on the shop page
-    test("verify presence of tiles on the shop page", async ({ page }) => {
+    test("verify the text 'Create your My Sky password' when user try to sign in with invalid credentials", async ({ page }) => {
       // Click [data-test-id="sign-in-link"]
       await Promise.all([
         page.locator('[data-test-id="sign-in-link"]').click(),
@@ -37,10 +37,6 @@ test.describe(
       // Click [data-testid="AUTHN__INPUT"]
       // await page.frameLocator('iframe[name="__zoid__sign_in_iframe__eyJ1aWQiOiJ6b2lkLXNpZ24taW4taWZyYW1lLWMwZjQyOWIyYTZfbXRhNm5kaTZtemUiLCJjb250ZXh0IjoiaWZyYW1lIiwidmVyc2lvbiI6IjlfMF8yNyIsImNoaWxkRG9tYWluIjoiaHR0cHM6Ly9hZ2cub29nd2F5LnNreS5jb20iLCJwYXJlbnREb21haW4iOiJodHRwczovL3d3dy5za3kuY29tIiwidGFnIjoic2lnbi1pbi1pZnJhbWUiLCJwYXJlbnQiOnsidHlwZSI6InBhcmVudCIsImRpc3RhbmNlIjowfSwicHJvcHMiOnsidHlwZSI6InJhdyIsInZhbHVlIjoie1wicHJvcG9zaXRpb25cIjpcIlNLWUNPTVwiLFwicHJvdmlkZXJcIjpcIlNLWVwiLFwiZGV2aWNlXCI6XCJDT01QVVRFUlwiLFwicGxhdGZvcm1cIjpcIlBDXCIsXCJ0ZXJyaXRvcnlcIjpcIkdCXCIsXCJvbkF1dGhvcml6ZWRcIjp7XCJfX3R5cGVfX1wiOlwiY3Jvc3NfZG9tYWluX2Z1bmN0aW9uXCIsXCJfX3ZhbF9fXCI6e1wiaWRcIjpcIjczMDc1YjAwZDNfbXRhNm5kaTZtemVcIixcIm5hbWVcIjpcIm9uQXV0aG9yaXplZFwifX0sXCJvbklkcmlzRXJyb3JcIjp7XCJfX3R5cGVfX1wiOlwiY3Jvc3NfZG9tYWluX2Z1bmN0aW9uXCIsXCJfX3ZhbF9fXCI6e1wiaWRcIjpcImIxYjFmMGIwYzRfbXRhNm5kaTZtemVcIixcIm5hbWVcIjpcIm9uSWRyaXNFcnJvclwifX0sXCJzaG93S2VlcE1lU2lnbmVkSW5cIjp0cnVlLFwiZXZlbnRzXCI6e1wib25QYWdlVHJhbnNpdGlvblwiOntcIl9fdHlwZV9fXCI6XCJjcm9zc19kb21haW5fZnVuY3Rpb25cIixcIl9fdmFsX19cIjp7XCJpZFwiOlwiZDhmZWU5YzhhOF9tdGE2bmRpNm16ZVwiLFwibmFtZVwiOlwib25QYWdlVHJhbnNpdGlvblwifX0sXCJvbkFjdGlvbkNsaWNrXCI6e1wiX190eXBlX19cIjpcImNyb3NzX2RvbWFpbl9mdW5jdGlvblwiLFwiX192YWxfX1wiOntcImlkXCI6XCIwNTVlNjg2MzZiX210YTZuZGk2bXplXCIsXCJuYW1lXCI6XCJvbkFjdGlvbkNsaWNrXCJ9fX0sXCJpc0ZyYW1lZFwiOlwidHJ1ZVwiLFwiaHJlZlwiOlwiaHR0cHM6Ly93d3cuc2t5LmNvbS9zaWduaW4\\/c3VjY2Vzc1VybD1odHRwcyUyNTNBJTI1MkYlMjUyRnd3dy5za3kuY29tJTI1MkYmY2FuY2VsVXJsPWh0dHBzJTI1M0ElMjUyRiUyNTJGd3d3LnNreS5jb20lMjUyRlwiLFwibWluaW1hbFByZXNlbnRhdGlvblwiOmZhbHNlLFwib3JpZ2luXCI6XCJodHRwczovL3d3dy5za3kuY29tXCIsXCJjbGllbnRJZFwiOlwiU0tZQ09NXCIsXCJyZWRpcmVjdFVyaVwiOlwiaHR0cHM6Ly9hY2NvdW50cy5za3kuY29tL3NpZ24taW4vZnJhbWUtYXV0aC1kb25lXCIsXCJyZXNwb25zZVR5cGVcIjpcImNvZGVcIixcImxvY2tJZGVudGlmaWVyXCI6ZmFsc2UsXCJjbGllbnRcIjpcImRlZmF1bHRcIn0ifSwiZXhwb3J0cyI6IntcImluaXRcIjp7XCJfX3R5cGVfX1wiOlwiY3Jvc3NfZG9tYWluX2Z1bmN0aW9uXCIsXCJfX3ZhbF9fXCI6e1wiaWRcIjpcIjQ3YjQ0MWUzMmZfbXRhNm5kaTZtemVcIixcIm5hbWVcIjpcInJcIn19LFwiY2xvc2VcIjp7XCJfX3R5cGVfX1wiOlwiY3Jvc3NfZG9tYWluX2Z1bmN0aW9uXCIsXCJfX3ZhbF9fXCI6e1wiaWRcIjpcIjJjNmFmNjUyZjJfbXRhNm5kaTZtemVcIixcIm5hbWVcIjpcImNsb3NlXCJ9fSxcImNoZWNrQ2xvc2VcIjp7XCJfX3R5cGVfX1wiOlwiY3Jvc3NfZG9tYWluX2Z1bmN0aW9uXCIsXCJfX3ZhbF9fXCI6e1wiaWRcIjpcImNiM2FhZTAyNGRfbXRhNm5kaTZtemVcIixcIm5hbWVcIjpcImNoZWNrQ2xvc2VcIn19LFwicmVzaXplXCI6e1wiX190eXBlX19cIjpcImNyb3NzX2RvbWFpbl9mdW5jdGlvblwiLFwiX192YWxfX1wiOntcImlkXCI6XCI2YzA1NGZjMWUxX210YTZuZGk2bXplXCIsXCJuYW1lXCI6XCJyZXNpemVcIn19LFwib25FcnJvclwiOntcIl9fdHlwZV9fXCI6XCJjcm9zc19kb21haW5fZnVuY3Rpb25cIixcIl9fdmFsX19cIjp7XCJpZFwiOlwiZjk2NTRlNjNiNl9tdGE2bmRpNm16ZVwiLFwibmFtZVwiOlwiblwifX19In0\\=__"]').locator('[data-testid="AUTHN__INPUT"]').click();
       await page.waitForTimeout(10000);
-
-      const handle = await page.$('.frame');
-      const contentFrame = await handle?.contentFrame();
-      console.log("contentFrame---",contentFrame)
       const locator = page
         .frameLocator(
           'iframe[name="__zoid__sign_in_iframe__eyJ1aWQiOiJ6b2lkLXNpZ24taW4taWZyYW1lLWMwZjQyOWIyYTZfbXRhNm5kaTZtemUiLCJjb250ZXh0IjoiaWZyYW1lIiwidmVyc2lvbiI6IjlfMF8yNyIsImNoaWxkRG9tYWluIjoiaHR0cHM6Ly9hZ2cub29nd2F5LnNreS5jb20iLCJwYXJlbnREb21haW4iOiJodHRwczovL3d3dy5za3kuY29tIiwidGFnIjoic2lnbi1pbi1pZnJhbWUiLCJwYXJlbnQiOnsidHlwZSI6InBhcmVudCIsImRpc3RhbmNlIjowfSwicHJvcHMiOnsidHlwZSI6InJhdyIsInZhbHVlIjoie1wicHJvcG9zaXRpb25cIjpcIlNLWUNPTVwiLFwicHJvdmlkZXJcIjpcIlNLWVwiLFwiZGV2aWNlXCI6XCJDT01QVVRFUlwiLFwicGxhdGZvcm1cIjpcIlBDXCIsXCJ0ZXJyaXRvcnlcIjpcIkdCXCIsXCJvbkF1dGhvcml6ZWRcIjp7XCJfX3R5cGVfX1wiOlwiY3Jvc3NfZG9tYWluX2Z1bmN0aW9uXCIsXCJfX3ZhbF9fXCI6e1wiaWRcIjpcIjczMDc1YjAwZDNfbXRhNm5kaTZtemVcIixcIm5hbWVcIjpcIm9uQXV0aG9yaXplZFwifX0sXCJvbklkcmlzRXJyb3JcIjp7XCJfX3R5cGVfX1wiOlwiY3Jvc3NfZG9tYWluX2Z1bmN0aW9uXCIsXCJfX3ZhbF9fXCI6e1wiaWRcIjpcImIxYjFmMGIwYzRfbXRhNm5kaTZtemVcIixcIm5hbWVcIjpcIm9uSWRyaXNFcnJvclwifX0sXCJzaG93S2VlcE1lU2lnbmVkSW5cIjp0cnVlLFwiZXZlbnRzXCI6e1wib25QYWdlVHJhbnNpdGlvblwiOntcIl9fdHlwZV9fXCI6XCJjcm9zc19kb21haW5fZnVuY3Rpb25cIixcIl9fdmFsX19cIjp7XCJpZFwiOlwiZDhmZWU5YzhhOF9tdGE2bmRpNm16ZVwiLFwibmFtZVwiOlwib25QYWdlVHJhbnNpdGlvblwifX0sXCJvbkFjdGlvbkNsaWNrXCI6e1wiX190eXBlX19cIjpcImNyb3NzX2RvbWFpbl9mdW5jdGlvblwiLFwiX192YWxfX1wiOntcImlkXCI6XCIwNTVlNjg2MzZiX210YTZuZGk2bXplXCIsXCJuYW1lXCI6XCJvbkFjdGlvbkNsaWNrXCJ9fX0sXCJpc0ZyYW1lZFwiOlwidHJ1ZVwiLFwiaHJlZlwiOlwiaHR0cHM6Ly93d3cuc2t5LmNvbS9zaWduaW4\\/c3VjY2Vzc1VybD1odHRwcyUyNTNBJTI1MkYlMjUyRnd3dy5za3kuY29tJTI1MkYmY2FuY2VsVXJsPWh0dHBzJTI1M0ElMjUyRiUyNTJGd3d3LnNreS5jb20lMjUyRlwiLFwibWluaW1hbFByZXNlbnRhdGlvblwiOmZhbHNlLFwib3JpZ2luXCI6XCJodHRwczovL3d3dy5za3kuY29tXCIsXCJjbGllbnRJZFwiOlwiU0tZQ09NXCIsXCJyZWRpcmVjdFVyaVwiOlwiaHR0cHM6Ly9hY2NvdW50cy5za3kuY29tL3NpZ24taW4vZnJhbWUtYXV0aC1kb25lXCIsXCJyZXNwb25zZVR5cGVcIjpcImNvZGVcIixcImxvY2tJZGVudGlmaWVyXCI6ZmFsc2UsXCJjbGllbnRcIjpcImRlZmF1bHRcIn0ifSwiZXhwb3J0cyI6IntcImluaXRcIjp7XCJfX3R5cGVfX1wiOlwiY3Jvc3NfZG9tYWluX2Z1bmN0aW9uXCIsXCJfX3ZhbF9fXCI6e1wiaWRcIjpcIjQ3YjQ0MWUzMmZfbXRhNm5kaTZtemVcIixcIm5hbWVcIjpcInJcIn19LFwiY2xvc2VcIjp7XCJfX3R5cGVfX1wiOlwiY3Jvc3NfZG9tYWluX2Z1bmN0aW9uXCIsXCJfX3ZhbF9fXCI6e1wiaWRcIjpcIjJjNmFmNjUyZjJfbXRhNm5kaTZtemVcIixcIm5hbWVcIjpcImNsb3NlXCJ9fSxcImNoZWNrQ2xvc2VcIjp7XCJfX3R5cGVfX1wiOlwiY3Jvc3NfZG9tYWluX2Z1bmN0aW9uXCIsXCJfX3ZhbF9fXCI6e1wiaWRcIjpcImNiM2FhZTAyNGRfbXRhNm5kaTZtemVcIixcIm5hbWVcIjpcImNoZWNrQ2xvc2VcIn19LFwicmVzaXplXCI6e1wiX190eXBlX19cIjpcImNyb3NzX2RvbWFpbl9mdW5jdGlvblwiLFwiX192YWxfX1wiOntcImlkXCI6XCI2YzA1NGZjMWUxX210YTZuZGk2bXplXCIsXCJuYW1lXCI6XCJyZXNpemVcIn19LFwib25FcnJvclwiOntcIl9fdHlwZV9fXCI6XCJjcm9zc19kb21haW5fZnVuY3Rpb25cIixcIl9fdmFsX19cIjp7XCJpZFwiOlwiZjk2NTRlNjNiNl9tdGE2bmRpNm16ZVwiLFwibmFtZVwiOlwiblwifX19In0\\=__"]'
@@ -60,7 +56,7 @@ test.describe(
     });
 
     // Scenario 3: User sees a list of deals on the deals page
-    test("verify list of deals on the deals page", async ({ page }) => {
+    test("verify list of deals list of deals with a price to it", async ({ page }) => {
       await page
         .locator('[data-test-id="primary-nav-list"] >> text=Deals')
         .click();
@@ -89,7 +85,6 @@ test.describe(
       await page.locator("text=Now, £38").click();
 
       await expect(page.locator("text=Now, £38")).toHaveText("Now, £38");
-
     });
   }
 );
